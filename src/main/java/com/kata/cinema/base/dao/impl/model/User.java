9fdc76a539b8487163d32073dab8c5d077098ace
lCarLyl <sujskskd@gmail.com>
2022-07-31 13:08:59 +0300

    creating a blank

--- src/main/java/com/kata/cinema/base/dao/impl/model/User.java
+++ src/main/java/com/kata/cinema/base/dao/impl/model/User.java
@@ -1,4 +1,13 @@
 package com.kata.cinema.base.dao.impl.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class User {
+
+    private String name;
+    private String lastname;
+
 }
