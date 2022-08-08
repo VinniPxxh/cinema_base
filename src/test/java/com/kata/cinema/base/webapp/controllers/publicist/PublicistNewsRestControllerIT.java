d73bcc8ebd99a324131a6e028197615e61c034ea
Whiskazzz <wilker2010@yandex.ru>
2022-08-08 16:39:23 +0300

    quick fix Test

--- src/test/java/com/kata/cinema/base/webapp/controllers/publicist/PublicistNewsRestControllerIT.java
+++ src/test/java/com/kata/cinema/base/webapp/controllers/publicist/PublicistNewsRestControllerIT.java
@@ -1,6 +1,6 @@
 package com.kata.cinema.base.webapp.controllers.publicist;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kata.cinema.base.AbstractIT;
 import com.kata.cinema.base.dao.abstracts.dto.NewsDao;
 import com.kata.cinema.base.models.dto.NewsRequestDto;
 import com.kata.cinema.base.models.enums.Rubric;
@@ -9,10 +9,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
+import static com.kata.cinema.base.AbstractIT.NEWS_REST_CONTROLLER_CLEAR_SQL;
+import static com.kata.cinema.base.AbstractIT.NEWS_REST_CONTROLLER_INIT_SQL;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -21,14 +24,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@Sql(value = "classpath:data/controller.publicist.sql/publicistNewsRestControllerInit.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(value = "classpath:data/controller.publicist.sql/publicistNewsRestControllerClear.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-class PublicistNewsRestControllerIT {
+@ActiveProfiles("IT")
+@Sql(value = NEWS_REST_CONTROLLER_INIT_SQL, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = NEWS_REST_CONTROLLER_CLEAR_SQL, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+class PublicistNewsRestControllerIT extends AbstractIT {
 
-    @Autowired
-    MockMvc mockMvc;
-    @Autowired
-    ObjectMapper objectMapper;
     @Autowired
     NewsDao newsDao;
 
@@ -45,6 +45,7 @@ class PublicistNewsRestControllerIT {
         this.mockMvc.perform(get("/api/publicist/news?startDate=2022-08-04&endDate=2022-08-05&rubric=NEWS"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(1)))
                 .andExpect(jsonPath("$.[0].rubric").value("NEWS"))
                 .andExpect(jsonPath("$.[0].date").value("2022-08-04"))
                 .andExpect(jsonPath("$.[0].title").value("db test title 02"))
@@ -56,6 +57,7 @@ class PublicistNewsRestControllerIT {
         this.mockMvc.perform(get("/api/publicist/news?startDate=2022-08-04&endDate=2022-08-05"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(2)))
                 .andExpect(jsonPath("$.[0].rubric").value("NEWS"))
                 .andExpect(jsonPath("$.[0].date").value("2022-08-04"))
                 .andExpect(jsonPath("$.[0].title").value("db test title 02"))
@@ -71,6 +73,7 @@ class PublicistNewsRestControllerIT {
         this.mockMvc.perform(get("/api/publicist/news?startDate=2022-08-04"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(2)))
                 .andExpect(jsonPath("$.[0].rubric").value("NEWS"))
                 .andExpect(jsonPath("$.[0].date").value("2022-08-04"))
                 .andExpect(jsonPath("$.[0].title").value("db test title 02"))
@@ -86,10 +89,15 @@ class PublicistNewsRestControllerIT {
         this.mockMvc.perform(get("/api/publicist/news?endDate=2022-08-04"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(2)))
                 .andExpect(jsonPath("$.[0].rubric").value("NEWS"))
                 .andExpect(jsonPath("$.[0].date").value("2022-08-03"))
                 .andExpect(jsonPath("$.[0].title").value("db test title 01"))
-                .andExpect(jsonPath("$.[0].description").value("nytipobody"));
+                .andExpect(jsonPath("$.[0].description").value("nytipobody"))
+                .andExpect(jsonPath("$.[1].rubric").value("NEWS"))
+                .andExpect(jsonPath("$.[1].date").value("2022-08-04"))
+                .andExpect(jsonPath("$.[1].title").value("db test title 02"))
+                .andExpect(jsonPath("$.[1].description").value("nytipobody"));
     }
 
     @Test
@@ -97,6 +105,7 @@ class PublicistNewsRestControllerIT {
         this.mockMvc.perform(get("/api/publicist/news?rubric=TESTS"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(1)))
                 .andExpect(jsonPath("$.[0].rubric").value("TESTS"))
                 .andExpect(jsonPath("$.[0].date").value("2022-08-05"))
                 .andExpect(jsonPath("$.[0].title").value("db test title 03"))
@@ -108,6 +117,7 @@ class PublicistNewsRestControllerIT {
         this.mockMvc.perform(get("/api/publicist/news?startDate=2022-08-04&rubric=NEWS"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(1)))
                 .andExpect(jsonPath("$.[0].rubric").value("NEWS"))
                 .andExpect(jsonPath("$.[0].date").value("2022-08-04"))
                 .andExpect(jsonPath("$.[0].title").value("db test title 02"))
@@ -119,6 +129,7 @@ class PublicistNewsRestControllerIT {
         this.mockMvc.perform(get("/api/publicist/news?endDate=2022-08-05&rubric=NEWS"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(2)))
                 .andExpect(jsonPath("$.[0].rubric").value("NEWS"))
                 .andExpect(jsonPath("$.[0].date").value("2022-08-03"))
                 .andExpect(jsonPath("$.[0].title").value("db test title 01"))
