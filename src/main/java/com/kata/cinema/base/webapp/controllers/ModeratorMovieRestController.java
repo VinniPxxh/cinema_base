16669ca237005b36899986faa0f82a36da53ca5b
punc1 <kuznetsove2001@mail.ru>
2022-08-09 14:27:09 +0300

    fix path to file

--- src/main/java/com/kata/cinema/base/webapp/controllers/ModeratorMovieRestController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/ModeratorMovieRestController.java
@@ -3,18 +3,20 @@ package com.kata.cinema.base.webapp.controllers;
 import com.kata.cinema.base.dao.impl.dto.AbstractDaoImpl;
 import com.kata.cinema.base.service.impl.entity.PreviewServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 @RestController()
 @RequestMapping("/api/moderator/movie")
 public class ModeratorMovieRestController extends AbstractDaoImpl<Long, MultipartFile> {
     private final PreviewServiceImpl previewService;
-
 
     @Autowired
     public ModeratorMovieRestController(PreviewServiceImpl previewService) {
@@ -24,8 +26,9 @@ public class ModeratorMovieRestController extends AbstractDaoImpl<Long, Multipar
 
     @PostMapping("/{id}/uploadPreview")
     public File uploadPreview(@PathVariable Long id, @RequestParam("file") MultipartFile file) throws IOException {
-        String path ="D:/Java Projects/cinema_base/uploads/movies/preview";
-        File conFile = new File(path + "\\" + file.getOriginalFilename());
+        Path relPath = Paths.get("uploads/movies/preview/");
+        Path absPath = relPath.toAbsolutePath();
+        File conFile = new File(absPath + "\\" + file.getOriginalFilename());
         System.out.println(conFile.getPath());
         try (FileOutputStream fos = new FileOutputStream(conFile)) {
             fos.write(file.getBytes());
