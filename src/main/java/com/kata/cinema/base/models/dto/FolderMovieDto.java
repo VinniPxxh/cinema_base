64ef21a98b444e93a705c5f23aa008f291e9d0d9
Саньяр Якупов <akupov671@gmail.com>
2022-08-14 09:07:57 +0000

    Update FolderMovieDto.java

--- src/main/java/com/kata/cinema/base/models/dto/FolderMovieDto.java
+++ src/main/java/com/kata/cinema/base/models/dto/FolderMovieDto.java
@@ -1,19 +1,1 @@
-package com.kata.cinema.base.models.dto;
 
-import com.kata.cinema.base.models.enums.Category;
-import com.kata.cinema.base.models.enums.Privacy;
-import com.kata.cinema.base.models.enums.Rubric;
-import lombok.*;
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class FolderMovieDto {
-
-    private Long id;
-    private Category category;
-    private Privacy privacy;
-    private String name;
-    private String description;
-}
