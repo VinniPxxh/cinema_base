ca1bda8049b8b5145ed84524c53994ab13e845e6
lCarLyl <sujskskd@gmail.com>
2022-08-22 16:58:41 +0300

    add AdviceController

---
+++ src/main/java/com/kata/cinema/base/webapp/controllers/ExceptionRestController.java
@@ -0,0 +1,15 @@
+package com.kata.cinema.base.webapp.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionRestController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> globalException(Exception exception){
+        return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
