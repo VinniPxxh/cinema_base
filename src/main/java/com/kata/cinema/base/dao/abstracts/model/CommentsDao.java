fe7af9d047b3c6387d7a826cd20292a60e60189d
zaborskiy <zaborskiy_ai@mail.ru>
2022-08-29 14:19:52 +0300

    фикс ендпоинт на коллекции

--- src/main/java/com/kata/cinema/base/dao/abstracts/model/CommentsDao.java
+++ src/main/java/com/kata/cinema/base/dao/abstracts/model/CommentsDao.java
@@ -1,7 +1,7 @@
 package com.kata.cinema.base.dao.abstracts.model;
 
 import com.kata.cinema.base.dao.abstracts.dto.AbstractDao;
-import com.kata.cinema.base.models.dto.GenreResponseDto;
+import com.kata.cinema.base.models.dto.response.GenreResponseDto;
 import com.kata.cinema.base.models.dto.response.CommentsResponseDto;
 import com.kata.cinema.base.models.entitys.Comments;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/kata/cinema/base/service/abstracts/model/CommentsService.java
+++ src/main/java/com/kata/cinema/base/service/abstracts/model/CommentsService.java
@@ -1,6 +1,6 @@
 package com.kata.cinema.base.service.abstracts.model;
 
-import com.kata.cinema.base.models.dto.GenreResponseDto;
+import com.kata.cinema.base.models.dto.response.GenreResponseDto;
 import com.kata.cinema.base.models.dto.response.CommentsResponseDto;
 import com.kata.cinema.base.models.entitys.Comments;
 import com.kata.cinema.base.models.entitys.Genres;
