bf7468baed18ed79639d7d1de0a3a2f296f13292
zaborskiy <zaborskiy_ai@mail.ru>
2022-08-30 13:43:19 +0300

    cors

--- src/main/java/com/kata/cinema/base/security/config/SecurityConfig.java
+++ src/main/java/com/kata/cinema/base/security/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
